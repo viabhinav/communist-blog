88a4e6efee3b488d50f15ddd49d6fc48bbe39cf7
viabhinav <viabhinav33@gmail.com>
2021-05-04 13:30:53 +0530

    Final

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect
 import pickledb as dbms
 import random as rand
 from werkzeug.exceptions import abort
+
 
 # REST API PORTABILITY
 
@@ -14,9 +15,6 @@ true = True
 # DATABASES CONFIG
 
 postdb = dbms.load("posts.json", True)
-postdb.dcreate("posts")
-postdb.dadd("posts", ("How to be a communist", 'The proletariat is the working-class; people who provide work to an employer in exchange for wages, but do not have any ownership of the company they work for or the "means of production," meaning the land, tools, factories, office buildings, raw materials, etc. that make their work possible. Most members of the proletariat have little control or say over their own labor, and do not share in the profits of their employer.[2]\nBecause the proletariat have no control over their labor and are dependent on wages to survive, they are easily exploited by their employers.\nThe oppressors of the proletariat are called the "bourgeoisie" in Marxist terms, the wealthy capitalists who own the corporations, factories, and land, and consequently, most of the worlds wealth.[3]\nThe modern concept of the 99% is very similar to Karl Marxs concept of the proletariat, and the 1% is analogous to the bourgeoisie.\nA key tenet of communism is that the proletariat should strive to gain control over the means of production, and own and manage it collectively.'))
-postdb.dadd("posts", ("How to be killed by TMC goons in WB!", "The art of being killed by goons, is very subtle, and involves joining a political party, which is the opposing party of the TMC. The exact process of doing this is to join BJP just before elections, and going to face a leader, and win. But do this ensuring TMC gets the government. Mission Accomplished!"))
 app = Flask("communist-blog")
 
 @app.route('/')
@@ -29,6 +27,7 @@ def viewposts():
     print(tuple(postdb.dgetall("posts")))
     posts = [(k, v) for k, v in (postdb.dgetall("posts")).items()]
     return render_template("viewposts.html", posts=posts)
+    postdb.dump()
 
 @app.route('/post/<string:postname>')
 def viewpost(postname):
@@ -36,18 +35,21 @@ def viewpost(postname):
         post = postdb.dget("posts", postname)
         print(postdb.dget("posts", postname))
         return render_template("viewpost.html", content=post, heading=postname)
+        postdb.dump()
     except Exception as e:
         abort(404)
 
 @app.route('/createpost')
 def new_post():
     return render_template("createpost.html")
+    postdb.dump()
 
 @app.route('/newpost', methods=['GET'])
 def newpost():
     try:
         postdb.dadd("posts", (str(request.args.get('heading')), str(request.args.get('content'))))
-        return render_template('viewposts.html')
+        return redirect('/posts', code=302)
+        postdb.dump()
     except:
         abort(404)
 
