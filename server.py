cf5f01e526979fde3518185269f68e1a9007228f
Abhinav Vishnu <>
2021-05-04 10:37:51 +0000

    Implemented Editor (Courtesy to editor.md)

--- server.py
+++ server.py
@@ -2,7 +2,14 @@ from flask import Flask, request, make_response, render_template, redirect
 import pickledb as dbms
 import random as rand
 from werkzeug.exceptions import abort
+from flaskext.markdown import Markdown as markdown
 
+
+def Reverse(tuples):
+    new_tup = ()
+    for k in reversed(tuples):
+        new_tup = new_tup + (k,)
+    return new_tup
 
 # REST API PORTABILITY
 
@@ -15,7 +22,9 @@ true = True
 # DATABASES CONFIG
 
 postdb = dbms.load("posts.json", True)
+#postdb.dcreate('posts')
 app = Flask("communist-blog")
+markdown(app)
 
 @app.route('/')
 def index():
@@ -26,7 +35,7 @@ def viewposts():
     print(postdb.dgetall("posts"))
     print(tuple(postdb.dgetall("posts")))
     posts = [(k, v) for k, v in (postdb.dgetall("posts")).items()]
-    return render_template("viewposts.html", posts=posts)
+    return render_template("viewposts.html", posts=Reverse(posts))
     postdb.dump()
 
 @app.route('/post/<string:postname>')
@@ -58,17 +67,24 @@ def newpost():
         postdb.dadd("posts", (str(request.args.get('heading')), str(request.args.get('content'))))
         return redirect('/posts', code=302)
         postdb.dump()
-    except:
-        abort(404)
+    except Exception as e:
+        print(e)
+        abort(400)
 
 @app.route('/edit-post', methods=['GET'])
 def editpost():
     try:
-        postdb.dpop("posts", request.args.get('oldhead'))
+        postdb.dpop("posts", request.args.get('oldheading'))
         postdb.dadd("posts", (str(request.args.get('heading')), str(request.args.get('content'))))
         return redirect('/posts', code=302)
         postdb.dump()
-    except Exception as e:
+    except KeyError as e:
+        print(e)
         return str(e)
+
+@app.route('/<string:post>/delete')
+def deletepost(post):
+    postdb.dpop("posts", post)
+    return redirect('/posts')
 
 app.run(host="0.0.0.0", port=1234)
