5b4cd0ea9090bc2ef5d5942275bc2ee406c6f1e7
Abhinav Vishnu <>
2021-05-04 09:16:43 +0000

    Implemented edit function

--- server.py
+++ server.py
@@ -36,7 +36,7 @@ def viewpost(postname):
         print(postdb.dget("posts", postname))
         return render_template("viewpost.html", content=post, heading=postname)
         postdb.dump()
-    except Exception as e:
+    except:
         abort(404)
 
 @app.route('/createpost')
@@ -44,6 +44,14 @@ def new_post():
     return render_template("createpost.html")
     postdb.dump()
 
+@app.route('/editpost', methods=["GET"])
+def edit_post():
+    try:
+        heading = request.args.get("heading")
+        return render_template("editpost.html",heading=heading, content=str(postdb.dget("posts",heading)))
+        postdb.dump()
+    except:
+        return 'Failure'
 @app.route('/newpost', methods=['GET'])
 def newpost():
     try:
@@ -52,5 +60,15 @@ def newpost():
         postdb.dump()
     except:
         abort(404)
+
+@app.route('/edit-post', methods=['GET'])
+def editpost():
+    try:
+        postdb.dpop("posts", request.args.get('oldhead'))
+        postdb.dadd("posts", (str(request.args.get('heading')), str(request.args.get('content'))))
+        return redirect('/posts', code=302)
+        postdb.dump()
+    except Exception as e:
+        return str(e)
 
 app.run(host="0.0.0.0", port=1234)
